57bcc86f3315b04a5729c9ecc189fff4dcc21213
unknown <parvezmrf@gmail.com>
2022-11-28 22:16:19 +0600

    banner img added

--- src/Pages/Home/Header/Slider.js
+++ src/Pages/Home/Header/Slider.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import img1 from '../../../assets/img1.png'
+import img2 from '../../../assets/img2.png'
+import img3 from '../../../assets/img3.png'
+import img4 from '../../../assets/img4.png'
 
 const Slider = () => {
     return (
         <div className='my-5' >
             <div className="carousel w-full">
                 <div id="item1" className="carousel-item w-full">
-                    <img src="https://placeimg.com/800/200/arch" className="w-full" alt='' />
+                    <img src={img1} className="w-full" alt='' />
                 </div>
                 <div id="item2" className="carousel-item w-full">
-                    <img src="https://placeimg.com/800/200/arch" className="w-full" alt='' />
+                    <img src={img2} className="w-full" alt='' />
                 </div>
                 <div id="item3" className="carousel-item w-full">
-                    <img src="https://placeimg.com/800/200/arch" className="w-full" alt='' />
+                    <img src={img3} className="w-full" alt='' />
                 </div>
                 <div id="item4" className="carousel-item w-full">
-                    <img src="https://placeimg.com/800/200/arch" className="w-full" alt='' />
+                    <img src={img4} className="w-full" alt='' />
                 </div>
             </div>
             <div className="flex justify-center w-full py-2 gap-2">
